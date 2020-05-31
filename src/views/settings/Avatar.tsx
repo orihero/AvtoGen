8e9857a3b85a9f62167b5238a74938ba2f9a876d
OriHero <mebungler@gmail.com>
2020-05-31 17:03:14 +0500

    Edit page

--- src/views/settings/Avatar.tsx
+++ src/views/settings/Avatar.tsx
@@ -1,35 +1,40 @@
-import React from 'react';
-import { View, Text, StyleSheet, Image } from 'react-native';
-import { Icons } from '../../constants/index';
-import { colors } from '../../constants/colors';
+import React from "react";
+import {
+	View,
+	Text,
+	StyleSheet,
+	Image,
+	TouchableWithoutFeedback
+} from "react-native";
+import { Icons } from "../../constants/index";
+import { colors } from "../../constants/colors";
 
-const Avatar = ({ image }) => {
-  return (
-    <View style={styles.container}>
-      <Image
-        style={styles.image}
-        source={{ uri: image }}
-      />
-      <View style={styles.pen}>
-        <Icons name={'pen'} color={colors.accent} size={18} />
-      </View>
-    </View>
-  );
+const Avatar = ({ image, onEdit }) => {
+	return (
+		<View style={styles.container}>
+			<Image style={styles.image} source={{ uri: image }} />
+			<TouchableWithoutFeedback onPress={onEdit}>
+				<View style={styles.pen}>
+					<Icons name={"pen"} color={colors.accent} size={18} />
+				</View>
+			</TouchableWithoutFeedback>
+		</View>
+	);
 };
 
 const styles = StyleSheet.create({
-  container: { justifyContent: 'center', alignItems: 'center' },
-  image: { width: 140, height: 140, borderRadius: 100 },
-  pen: {
-    backgroundColor: colors.extraGray,
-    borderRadius: 40,
-    width: 50,
-    height: 50,
-    justifyContent: 'center',
-    alignItems: 'center',
-    transform: [{ translateX: 40 }, { translateY: -40 }],
-    marginBottom: -30
-  },
+	container: { justifyContent: "center", alignItems: "center" },
+	image: { width: 140, height: 140, borderRadius: 100 },
+	pen: {
+		backgroundColor: colors.extraGray,
+		borderRadius: 40,
+		width: 50,
+		height: 50,
+		justifyContent: "center",
+		alignItems: "center",
+		transform: [{ translateX: 40 }, { translateY: -40 }],
+		marginBottom: -30
+	}
 });
 
 export default Avatar;
